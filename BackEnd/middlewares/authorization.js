8d31fdee9a2649ab2823299c180bb191448bf900
Bhushan-20 <bhushan72845@gmail.com>
2024-02-25 00:58:12 +0530

    Updated Authorization and few changes made in mailSender

--- BackEnd/middlewares/authorization.js
+++ BackEnd/middlewares/authorization.js
@@ -1,96 +1,99 @@
-const User = require("../models/User");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const User = require("../models/User");
 
-//authorization
-exports.auth = async(req,res,next) => {
+//auth
+exports.auth = async (req, res, next) => {
     try{
         //extract token
-        const token = req.cookies.token || req.body.token || req.header("Authorization").replace("Bearer","");
+        const token = req.cookies.token 
+                        || req.body.token 
+                        || req.header("Authorisation").replace("Bearer ", "");
 
-        if(!token){
+        //if token missing, then return response
+        if(!token) {
             return res.status(401).json({
                 success:false,
-                message:"Token is missing"
-            })
+                message:'TOken is missing',
+            });
         }
 
-        //verify token
+        //verify the token
         try{
-            const decode = jwt.verify(token,process.env.JWT_SECRET);
+            const decode =  jwt.verify(token, process.env.JWT_SECRET);
             console.log(decode);
             req.user = decode;
         }
-        catch(err){
+        catch(err) {
+            //verification - issue
             return res.status(401).json({
                 success:false,
-                message:"Invalid Token"
-            })
+                message:'token is invalid',
+            });
         }
         next();
     }
-    
-    catch(error){
+    catch(error) {  
         return res.status(401).json({
             success:false,
-            message:"Something went Wrong while validating token"
-        })
+            message:'Something went wrong while validating the token',
+        });
     }
 }
 
 //isStudent
-exports.isStudent = async (req,res,next) => {
-    try{
-        if(req.user.accountType !== "Student"){
+exports.isStudent = async (req, res, next) => {
+ try{
+        if(req.user.accountType !== "Student") {
             return res.status(401).json({
                 success:false,
-                message:"This is Protected Route for Students Only"
+                message:'This is a protected route for Students only',
             });
         }
         next();
-    }
-    catch(error){
-        return res.status(500).json({
-            success:false,
-            message:"User Role Cannot verified,Try Again After some time"
-        })
-    }
+ }
+ catch(error) {
+    return res.status(500).json({
+        success:false,
+        message:'User role cannot be verified, please try again'
+    })
+ }
 }
 
 //isInstructor
-exports.isInstructor = async (req,res,next) => {
+exports.isInstructor = async (req, res, next) => {
     try{
-        if(req.user.accountType !== "Instructor"){
-            return res.status(401).json({
-                success:false,
-                message:"This is Protected Route for Instructor Only"
-            });
-        }
-        next();
+           if(req.user.accountType !== "Instructor") {
+               return res.status(401).json({
+                   success:false,
+                   message:'This is a protected route for Instructor only',
+               });
+           }
+           next();
     }
-    catch(error){
-        return res.status(500).json({
-            success:false,
-            message:"User Role Cannot verified,Try Again After some time"
-        })
+    catch(error) {
+       return res.status(500).json({
+           success:false,
+           message:'User role cannot be verified, please try again'
+       })
     }
-}
+   }
 
 //isAdmin
-exports.isAdmin = async (req,res,next) => {
+exports.isAdmin = async (req, res, next) => {
     try{
-        if(req.user.accountType !== "Admin"){
-            return res.status(401).json({
-                success:false,
-                message:"This is Protected Route for Admin Only"
-            });
-        }
-        next();
+           if(req.user.accountType !== "Admin") {
+               return res.status(401).json({
+                   success:false,
+                   message:'This is a protected route for Admin only',
+               });
+           }
+           next();
     }
-    catch(error){
-        return res.status(500).json({
-            success:false,
-            message:"User Role Cannot verified,Try Again After some time"
-        })
+    catch(error) {
+       return res.status(500).json({
+           success:false,
+           message:'User role cannot be verified, please try again'
+       })
     }
-}
+   }
--- BackEnd/utils/mailSender.js
+++ BackEnd/utils/mailSender.js
@@ -1,30 +1,29 @@
-const nodemailer = require("node-mailer");
-require("dotenv").config();
+const nodemailer = require("nodemailer");
 
-const mailSender = async(email,title,body) => {
-
+const mailSender = async (email, title, body) => {
     try{
-        let transporter = nodemailer.createTransport({
-            host:process.env.MAIL_HOST,
-            auth:{
-                user:process.env.MAIL_USER,
-                pass: process.env.MAIL_PASS,
-            }
-            
-        });
+            let transporter = nodemailer.createTransport({
+                host:process.env.MAIL_HOST,
+                auth:{
+                    user: process.env.MAIL_USER,
+                    pass: process.env.MAIL_PASS,
+                }
+            })
 
-        let info = await transporter.sendMail({
-            from: 'From Bhushan Patil',
-            to: `${email}`,
-            subject:`${title}`,
-            html:`${body}`,
-        })
-        console.log(info);
-        return info;
+
+            let info = await transporter.sendMail({
+                from: 'StudyNotion || From Bhushan Patil',
+                to:`${email}`,
+                subject: `${title}`,
+                html: `${body}`,
+            })
+            console.log(info);
+            return info;
     }
-    catch(error){
+    catch(error) {
         console.log(error.message);
     }
 }
 
-module.exports = mailSender
+
+module.exports = mailSender;
