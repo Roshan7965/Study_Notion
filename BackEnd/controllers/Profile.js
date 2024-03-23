a86a4b2ead07eb6ba0ecb24b1f02a4dd262f9a49
Bhushan-20 <bhushan72845@gmail.com>
2024-03-24 00:59:25 +0530

    updated some minor error in Update password code and authorization part

--- BackEnd/controllers/Profile.js
+++ BackEnd/controllers/Profile.js
@@ -18,7 +18,7 @@ exports.updateProfile = async (req, res) => {
 		}
 
 		// Find or create the profile
-		let profile = await Profile.findById(userDetails.additionalDetails);
+		const profile = await Profile.findById(userDetails.additionalDetails);
 		if (!profile) {
 			profile = new Profile();
 			// Optionally set any default values for the new profile here
@@ -32,11 +32,16 @@ exports.updateProfile = async (req, res) => {
 		// Save the updated profile
 		await profile.save();
 
-		return res.json({
-			success: true,
-			message: "Profile updated successfully",
-			profile,
-		});
+		// Find the updated user details
+		const updatedUserDetails = await User.findById(id)
+		.populate("additionalDetails")
+		.exec()
+  
+	  return res.json({
+		success: true,
+		message: "Profile updated successfully",
+		updatedUserDetails,
+	  })
 	} catch (error) {
 		console.log(error);
 		return res.status(500).json({
@@ -153,3 +158,31 @@ exports.getEnrolledCourses = async (req, res) => {
       })
     }
 };
+
+exports.instructorDashboard = async (req, res) => {
+	try {
+	  const courseDetails = await Course.find({ instructor: req.user.id })
+  
+	  const courseData = courseDetails.map((course) => {
+		const totalStudentsEnrolled = course.studentsEnroled.length
+		const totalAmountGenerated = totalStudentsEnrolled * course.price
+  
+		// Create a new object with the additional fields
+		const courseDataWithStats = {
+		  _id: course._id,
+		  courseName: course.courseName,
+		  courseDescription: course.courseDescription,
+		  // Include other course properties as needed
+		  totalStudentsEnrolled,
+		  totalAmountGenerated,
+		}
+  
+		return courseDataWithStats
+	  })
+  
+	  res.status(200).json({ courses: courseData })
+	} catch (error) {
+	  console.error(error)
+	  res.status(500).json({ message: "Server Error" })
+	}
+  }
