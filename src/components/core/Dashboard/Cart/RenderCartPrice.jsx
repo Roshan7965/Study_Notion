795defe65f0db2ae70ad0a52bcf8d230434e5350
Bhushan-20 <bhushan72845@gmail.com>
2024-04-07 16:39:19 +0530

    Added Cart page where courses which are added in cart are shown

--- src/components/core/Dashboard/Cart/RenderCartPrice.jsx
+++ src/components/core/Dashboard/Cart/RenderCartPrice.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import IconBtn from '../../../common/IconBtn';
-// import { purchaseDirectly } from '../../../../services/operations/studentFeaturesAPI';
-import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
 
-const RenderCartPrice = () => {
-  const {total, cart} = useSelector((state) => state.cart);
+import IconBtn from "../../../common/IconBtn"
+import { buyCourse } from "../../../../services/operations/studentFeaturesAPI"
+
+export default function RenderTotalAmount() {
+  const { total, cart } = useSelector((state) => state.cart)
   const { token } = useSelector((state) => state.auth)
-  // const { user } = useSelector((state) => state.profile)
+  const { user } = useSelector((state) => state.profile)
   const navigate = useNavigate()
   const dispatch = useDispatch()
-  const handleBuyCourse = async () => {
-    const courses = cart.map((course) => course._id);
-    // await BuyCourse(token, [courseId], user, navigate, dispatch)
-    // await purchaseDirectly({courses}, token, navigate, dispatch)
+
+  const handleBuyCourse = () => {
+    const courses = cart.map((course) => course._id)
+    buyCourse(token, courses, user, navigate, dispatch)
   }
+
   return (
     <div className="min-w-[280px] rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-6">
       <p className="mb-1 text-sm font-medium text-richblack-300">Total:</p>
@@ -27,5 +28,3 @@ const RenderCartPrice = () => {
     </div>
   )
 }
-
-export default RenderCartPrice
