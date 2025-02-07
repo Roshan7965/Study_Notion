ef74515bbf1849695efa5429dd693c13ca9e1b41
Roshan Tanpure <141671667+Roshan7965@users.noreply.github.com>
2025-02-07 11:13:21 +0530

    Update mailSender.js

--- BackEnd/utils/mailSender.js
+++ BackEnd/utils/mailSender.js
@@ -15,7 +15,7 @@ const mailSender = async (email, title, body) => {
             })
 
             let info = await transporter.sendMail({
-                from: 'StudyNotion || From Bhushan Patil ',
+                from: 'StudyNotion || From Roshan Tanpure ',
                 to:`${email}`,
                 subject: `${title}`,
                 html: `${body}`,
